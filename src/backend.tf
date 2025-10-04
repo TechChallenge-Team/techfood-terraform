9f7767de6c15762a5cb65919bd2917174b4ec2e8
leandro oliveira cervantes <leandro_10_cervantes@hotmail.com>
2025-10-03 23:54:36 -0300

    Update S3 bucket references to use 'techfood-bucket' for consistency

--- src/backend.tf
+++ src/backend.tf
@@ -1,6 +1,6 @@
     terraform {
       backend "s3" {
-        bucket         = "techfood"
+        bucket         = "techfood-bucket"
         key            = "terraform/terraform.tfstate"
         region         = "us-east-1"
         encrypt        = true
