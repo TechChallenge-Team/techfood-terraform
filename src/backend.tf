09b293e1b3ae1f03552007e63d0319a60e4afbb9
leandro oliveira cervantes <leandro_10_cervantes@hotmail.com>
2025-10-04 00:39:13 -0300

    Update S3 bucket configuration for Terraform backend and outputs

--- src/backend.tf
+++ src/backend.tf
@@ -1,7 +1,7 @@
     terraform {
       backend "s3" {
-        bucket         = "techfood-bucket"
-        key            = "terraform/terraform.tfstate"
+        bucket         = "techfood-tf-bucket"
+        key            = "terraform.tfstate"
         region         = "us-east-1"
         encrypt        = true
       }
