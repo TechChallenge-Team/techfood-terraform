d0602cb7225cef08afb8f4d8b9921d985706ae16
leandro oliveira cervantes <leandro_10_cervantes@hotmail.com>
2025-10-05 15:12:29 -0300

    fix: update S3 bucket name format for lambda artifacts

--- src/bucket.tf
+++ src/bucket.tf
@@ -4,8 +4,10 @@
 #}
 
 resource "aws_s3_bucket" "lambda_artifacts" {
-  bucket        = lower("${var.projectName}-lambda-artifacts-${data.aws_caller_identity.current.account_id}-${var.region_default}")
+  bucket        = "${var.projectName}-lambda-artifacts-bucket"
   force_destroy = true
 
-  tags = merge(var.tags, { Service = "lambda-artifacts" })
+  tags = merge(var.tags, {
+    Service = "lambda-artifacts" 
+  })
 }
