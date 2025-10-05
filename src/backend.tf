22faa7e9793fdd0e1ff5155b92a164ec180a271a
leandro oliveira cervantes <leandro_10_cervantes@hotmail.com>
2025-10-05 19:39:35 -0300

    fix: update S3 bucket name in backend configuration

--- src/backend.tf
+++ src/backend.tf
@@ -1,6 +1,6 @@
     terraform {
       backend "s3" {
-        bucket         = "techfood-terraform-bucket"
+        bucket         = "techfood-tf-bucket"
         key            = "terraform.tfstate"
         region         = "us-east-1"
         encrypt        = true
