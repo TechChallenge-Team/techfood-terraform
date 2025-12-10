60af4b6818c89a1af5be4eaf7476eae7204c1c07
paulo <paulo@PHPC>
2025-12-09 21:30:13 -0300

    Update S3 bucket name in backend configuration for Terraform

--- src/backend.tf
+++ src/backend.tf
@@ -1,6 +1,6 @@
     terraform {
       backend "s3" {
-        bucket         = "techfood-tf-bucket"
+        bucket         = "techfood-tf-bucket-techchallenge"
         key            = "terraform.tfstate"
         region         = "us-east-1"
         encrypt        = true
